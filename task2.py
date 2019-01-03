93e8e73eae2ec78966e40cae350f1261cdbdcf11
Artem Uskov <artem.uskov@bazaarvoice.com>
2019-01-03 14:01:27 +0200

    Add input checking

--- task2.py
+++ task2.py
@@ -8,6 +8,13 @@ parser.add_argument('end', type=int, help='end of the range')
 args = parser.parse_args()
 
 
+if args.start >= args.end:
+    parser.error('"start" number must be less then "end"')
+
+if isinstance(args.start, int) and isinstance(args.end, int) is not True:
+    parser.error('input parameters must be integer')
+
+
 def summarize(x, y):
     return x + y
 

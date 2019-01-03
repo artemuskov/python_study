4fc67b2763650536e25de6c601899cb426198144
Artem Uskov <artem.uskov@bazaarvoice.com>
2019-01-03 14:11:37 +0200

    Change loop for elif/else constraction

--- task1.py
+++ task1.py
@@ -2,18 +2,15 @@
 
 
 def devide_by_number(inpt, dev):
-    if inpt % dev == 0:
-        return True
+    return inpt % dev == 0
 
 
 for i in range(1, 101):
     if devide_by_number(i, 15):
-        print "fizzbizz"
-        continue
-    if devide_by_number(i, 5):
-        print "bizz"
-        continue
-    if devide_by_number(i, 3):
+        print "fizzbuzz"
+    elif devide_by_number(i, 5):
+        print "buzz"
+    elif devide_by_number(i, 3):
         print "fizz"
-        continue
-    print i
+    else:
+        print i

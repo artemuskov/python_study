5b89e2b1c2da3be3eca6634187f1bc99c04d71ef
Artem Uskov <artem.uskov@bazaarvoice.com>
2019-01-03 16:47:28 +0200

    test

--- task4.py
+++ task4.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 
+import urllib
 import urllib2
 
 
 def main():
-    req = urllib2.Request('http://ip.jsontest.com/')
-    res = urllib2.urlopen(req)
-    print res.info()
-    res.close();
+
+    req = urllib2.Request('http://www.google.com/')
+    with urllib2.urlopen(req) as resp:
+        print 123
+
+
+
+
+    # with request.urlopen('http://www.google.com/') as f:
+    #     print(f.getcode())  # http response code
+    #     # print(f.info())  # all header info
+    #     #
+    #     # resp_body = f.read().decode('utf-8')  # response body
 
 
 if __name__ == "__main__":
